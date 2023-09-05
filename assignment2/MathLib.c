470a69fa8a6bf3f0bb861538f05ca9bc392c5f5f
yousufelshrif <92643469+yousufelshrif@users.noreply.github.com>
2023-09-05 20:14:00 +0300

    Delete assignment2 directory

--- assignment2/MathLib.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-unsigned char_Add(unsigned char1,unsigned char2)
-{
-
-
-     return char1 + char2;
-}
--- assignment2/MathLib.h
+++
@@ -1,6 +0,0 @@
-#ifndef MATHLIB_H_INCLUDED
-#define MATHLIB_H_INCLUDED
-unsigned char_Add(unsigned char1,unsigned char2)
-
-
-#endif // MATHLIB_H_INCLUDED
